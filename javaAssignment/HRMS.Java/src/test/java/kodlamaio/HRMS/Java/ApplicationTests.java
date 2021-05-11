4dfb3b4cda6d852725cb7fc938bb487a6189f31e
Ceyda Kamalı <ceydakamali3@gmail.com>
2021-05-11 19:15:33 +0300

    lecture 6 - assignment 3

---
+++ javaAssignment/HRMS.Java/src/test/java/kodlamaio/HRMS/Java/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kodlamaio.HRMS.Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
